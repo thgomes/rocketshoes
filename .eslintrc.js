e868c4cd17c8808ea069f1a6d7e75497027d63b2
Thiago Gomes <thiagosiqgomes@gmail.com>
2020-05-06 01:54:09 -0300

    replace classes for functions, using react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -34,6 +35,8 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
     'no-console': 'off',
-    'no-param-reassign': 'off'
+    'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -8,13 +8,14 @@ import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-function Header({ cartSize }) {
+export default function Header() {
+  const cartSize = useSelector((state) => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
         <img src={logo} alt="Rocketshoes" />
       </Link>
-
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
@@ -25,7 +26,3 @@ function Header({ cartSize }) {
     </Container>
   );
 }
-
-export default connect((state) => ({
-  cartSize: state.cart.length,
-}))(Header);
